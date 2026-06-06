e75ff7c00f2217f07d3080cfedf47b1dab587603
EricChv <101233505+EricChv@users.noreply.github.com>
2026-06-06 15:50:03 -0400

    feat: add homepage hero section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume AI" },
+    { name: "description", content: "Upload resumes, create job listings, and use AI to instantly analyze candidate fit." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  // When using strings inside [url()] use double quotes
+  return <main className="bg-[url('/images/bg-main.svg')]">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Resume Analysis & Application Tracker</h1>
+        <h2>Get AI-powered resume feedback and track every job application in one place.</h2>
+
+      </div>
+    </section>
+  </ main>
 }
